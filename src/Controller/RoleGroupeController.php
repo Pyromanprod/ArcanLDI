fb2bae46f1a23c3c227f0ba289c6356f109ca0ec
Hypno <hypno@hypno.hypno>
2021-10-26 16:45:08 +0200

    roulegroupe crud

--- src/Controller/RoleGroupeController.php
+++ src/Controller/RoleGroupeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Entity\RoleGroupe;
+use App\Entity\User;
+use App\Form\RoleAddFormType;
 use App\Form\RoleGroupeType;
+use App\Repository\OrderRepository;
 use App\Repository\RoleGroupeRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -67,6 +72,29 @@ class RoleGroupeController extends AbstractController
             'form' => $form,
         ]);
     }
+    //ajout role de groupe a un utilisateur
+    #[Route('/{id}/add', name: 'role_groupe_add', methods: ['GET','POST'])]
+    public function add(Request $request, User $user, OrderRepository $orderRepository): Response
+    {
+
+        $form = $this->createForm(RoleAddFormType::class);
+        $form->handleRequest($request);
+        $id = $orderRepository->findOneByPlayer($user)->getTicket()->getId();
+        if ($form->isSubmitted() && $form->isValid()) {
+            foreach ($form->get('name')->getData() as $role){
+            $user->addRoleGroupe($role);
+            }
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_index',[
+                'id' => $id
+            ], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('role_groupe/_add.html.twig', [
+            'form' => $form,
+        ]);
+    }
 
     #[Route('/{id}', name: 'role_groupe_delete', methods: ['POST'])]
     public function delete(Request $request, RoleGroupe $roleGroupe): Response
@@ -78,5 +106,30 @@ class RoleGroupeController extends AbstractController
         }
 
         return $this->redirectToRoute('role_groupe_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+    #[Route('/delete/{pseudo}-{id}', name: 'role_delete', methods: ['POST','GET'])]
+//    #[Entity('user', expr: 'repository.find(pseudo)')]
+    #[ParamConverter('user', options: ['mapping' => ['pseudo' => 'pseudo']])]
+    #[ParamConverter('roleGroupe', options: ['mapping' => ['id' => 'id']])]
+    public function deleteRole(User $user,RoleGroupe $roleGroupe, Request $request): Response
+    {
+        dump($user);
+        $entityManager = $this->getDoctrine()->getRepository(Order::class);
+        $id = $entityManager->findOneByPlayer($user)->getTicket()->getId();
+
+        if ($this->isCsrfTokenValid('delete'.$roleGroupe->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->removeRoleGroupe($roleGroupe);
+            $entityManager->flush();
+        } else{
+            $this->addFlash('error','role non retirer');
+        }
+
+        return $this->redirectToRoute('user_index', [
+            'id' => $id
+
+        ], Response::HTTP_SEE_OTHER);
     }
 }
