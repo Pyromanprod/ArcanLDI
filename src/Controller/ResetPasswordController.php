ce284a8f52a431287808e9c1b11391cdc4112f3d
Hypno <hypno@hypno.hypno>
2021-11-24 11:52:37 +0100

    ajout reinitialisation mot de passe + js

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -133,7 +133,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_logout');
         }
 
         return $this->render('reset_password/reset.html.twig', [
